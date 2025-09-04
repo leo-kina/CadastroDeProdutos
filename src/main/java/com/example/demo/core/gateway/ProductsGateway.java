caa9343caf416d0468f9ad7bb888507caeec06fe
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-04 11:23:19 -0300

    feat: criando alguns metodos do gateway e criando productsentity

--- src/main/java/com/example/demo/core/gateway/ProductsGateway.java
+++ src/main/java/com/example/demo/core/gateway/ProductsGateway.java
@@ -1,4 +1,13 @@
 package com.example.demo.core.gateway;
 
+import com.example.demo.core.entities.Products;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ProductsGateway {
+    Products criarProduto(Products products);
+    List<Products> listarProdutos();
+    Optional<Products> acharPorIdentificador(UUID identifier);
 }
--- src/main/java/com/example/demo/infra/persistence/ProductsEntity.java
+++ src/main/java/com/example/demo/infra/persistence/ProductsEntity.java
@@ -1,4 +1,29 @@
-package com.example.demo.infra;
+package com.example.demo.infra.persistence;
 
+import com.example.demo.core.enums.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
 public class ProductsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @Column(unique = true)
+    private UUID identifier;
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private Integer stock;
 }
