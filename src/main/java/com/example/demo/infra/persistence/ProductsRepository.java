a0d4cfb6b6b4180d9c3f76cde5a15188b491a846
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-04 11:12:16 -0300

    feat: criando repository

--- src/main/java/com/example/demo/infra/persistence/ProductsRepository.java
+++ src/main/java/com/example/demo/infra/persistence/ProductsRepository.java
@@ -1,4 +1,11 @@
 package com.example.demo.infra.persistence;
 
-public interface ProductsRepository {
+import com.example.demo.core.entities.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProductsRepository extends JpaRepository<ProductsEntity, Long > {
+    Optional<Products> findByIdentificador(UUID identificador);
 }
