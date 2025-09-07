ca5277498ba3ca971e65400043643a87a6c1254e
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-06 22:27:52 -0300

    feat: criando requisisao de get

--- src/main/java/com/example/demo/infra/presentation/ProductsController.java
+++ src/main/java/com/example/demo/infra/presentation/ProductsController.java
@@ -1,27 +1,37 @@
 package com.example.demo.infra.presentation;
 
 import com.example.demo.core.entities.Products;
+import com.example.demo.core.usecases.CreateProductsUsecase;
 import com.example.demo.core.usecases.ListProductsUsecase;
 import com.example.demo.infra.dtos.ProductsDTO;
 import com.example.demo.infra.mapper.ProductDtoMapper;
 import com.example.demo.infra.persistence.ProductsEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("produtos")
 public class ProductsController {
     private final ProductDtoMapper productsDTO;
     private final ListProductsUsecase listProductsUsecase;
+    private final CreateProductsUsecase createProductsUsecase;
 
-    public ProductsController(ProductDtoMapper productsDTO, ListProductsUsecase listProductsUsecase) {
+    public ProductsController(ProductDtoMapper productsDTO, ListProductsUsecase listProductsUsecase, CreateProductsUsecase createProductsUsecase) {
         this.productsDTO = productsDTO;
         this.listProductsUsecase = listProductsUsecase;
+        this.createProductsUsecase = createProductsUsecase;
     }
 
-    @GetMapping("/litar")
+    @GetMapping("/listar")
     public List<ProductsDTO> listarprodutos(){
         return listProductsUsecase.execute().stream().map(productsDTO::toDto).toList();
     }
 
+    @PostMapping("/criar")
+    public ProductsDTO criarprodutos(@RequestBody ProductsDTO products){
+        Products novoproduto = createProductsUsecase.excute(productsDTO.toDomain(products));
+        return productsDTO.toDto(novoproduto);
+
+
+    }
 }
