58471e9b7bec5d5cc0242af6933e13538a4c7083
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-06 22:26:05 -0300

    feat: adicioanndo logica do bean

--- src/main/java/com/example/demo/infra/beans/BeanCOnfiguration.java
+++ src/main/java/com/example/demo/infra/beans/BeanCOnfiguration.java
@@ -1,4 +1,21 @@
 package com.example.demo.infra.beans;
 
+import com.example.demo.core.gateway.ProductsGateway;
+import com.example.demo.core.usecases.CreateProductsUsecase;
+import com.example.demo.core.usecases.CreateProductsUsecaseImpl;
+import com.example.demo.core.usecases.ListProductsUsecase;
+import com.example.demo.core.usecases.ListProductsUsecaseImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BeanCOnfiguration {
+    @Bean
+    public CreateProductsUsecase createProductsUsecase (ProductsGateway productsGateway){
+        return new CreateProductsUsecaseImpl(productsGateway);
+    }
+    @Bean
+    public ListProductsUsecase listProductsUsecase (ProductsGateway productsGateway){
+        return new ListProductsUsecaseImpl(productsGateway);
+    }
 }
